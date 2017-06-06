b6008111958788629d5f1e418615e554b5c3c97f
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-06-06 17:55:03 +0200

    Add methods

--- src/org/main/First.groovy
+++ src/org/main/First.groovy
@@ -1,5 +1,13 @@
 package org.main;
 
 class First {
-   float x, y, z 
+   float x = 10, y = 5, z = 1
+
+   int sumUp() {
+       return x + y + z
+   } 
+
+   int printSth() {
+       println sumUp()
+   }
 }
