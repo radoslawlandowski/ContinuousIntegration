5e06d4ad784e14bafda571d3afb4427b357916aa
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-06-06 17:10:53 +0200

    Add function

--- src/org/main/First.groovy
+++ src/org/main/First.groovy
@@ -1,6 +1,7 @@
+package org.main;
+
 class Example {
-   static void main(String[] args) {
-      // Using a simple println statement to print output to the console
-      println('Hello World');
+   def Hello() {
+        println "Hello!"
    }
 }
