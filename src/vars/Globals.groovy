1247b6fdb43931b89ec4f9ef724d5e001271224c
radoslawlandowski <radoslaw.landowski@gmail.com>
2017-07-02 13:00:49 +0200

    asd

--- src/vars/Globals.groovy
+++ src/vars/Globals.groovy
@@ -1,1 +1,15 @@
-def myself = "RADEX"
+// vars/acme.groovy
+
+class Globals implements Serializable {
+    private String name
+    
+    def setName(value) {
+        name = value
+    }
+    def getName() {
+        name
+    }
+    def caution(message) {
+        echo "Hello, ${name}! CAUTION: ${message}"
+    }
+}
